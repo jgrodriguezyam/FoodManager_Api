ccc8e32212ec45d1e5e2d57df065068fd0cd19c2
jorge_rodriguez <jrodriguezy@bepensa.com>
2016-05-23 14:54:46 -0500

    Change  property name to commetnt in MenuDto Add property Portion to Reservation and remove NutritionalInformation Remove validation DayWeek of MenuValidator

--- FoodManager.DTO/Reservation.cs
+++ FoodManager.DTO/Reservation.cs
@@ -1,14 +1,13 @@
-﻿using FoodManager.Infrastructure.Application;
-
-namespace FoodManager.DTO
+﻿namespace FoodManager.DTO
 {
-    public class Reservation : NutritionInformation
+    public class Reservation
     {
         public int Id { get; set; }
         public string Date { get; set; }
         public Worker Worker { get; set; }
         public Saucer Saucer { get; set; }
         public Dealer Dealer { get; set; }
+        public decimal Portion { get; set; }
         public bool Status { get; set; }
     }
 }
